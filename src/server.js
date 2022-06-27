5f3b469fecfa4f754ab8a79bed509d53886bfd90
mymasyhar <mymasyhar@gmail.com>
2022-06-27 15:17:42 +0700

    test pushing with mymasyhar@gmail.com

---
+++ src/server.js
@@ -0,0 +1,20 @@
+import Hapi from '@hapi/hapi';
+import routes from './routes.js';
+
+const init = async () => {
+    const server = Hapi.server({
+        host: 'localhost',
+        port: 5000,
+        "routes": {
+            "cors": {
+                "origin" : ["*"],
+            },
+        },
+    })
+    server.route(routes);
+
+    await server.start();
+    console.log(`server running at : ${server.info.uri}`);
+}
+
+init();
