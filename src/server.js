bbe0c1317eba0afbc735b2e5fdf9bd78a2b806a0
mymasyhar <mymasyhar@gmail.com>
2022-06-28 16:09:31 +0700

    fixing

--- src/server.js
+++ src/server.js
@@ -3,7 +3,7 @@ import routes from './routes.js';
 
 const init = async () => {
     const server = Hapi.server({
-        host: process.env.NODE_ENV !== "production" ? "localhost" : "0.0.0.0",
+        host: process.env.NODE_ENV !== 'production' ? 'localhost' : '0.0.0.0',
         port: 5000,
         "routes": {
             "cors": {
